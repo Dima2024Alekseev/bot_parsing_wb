3819dcabd567bd86c9b49a39901473801ae227f1
Dima2024Alekseev <alekseevdimas873@gmail.com>
2025-08-23 12:07:02 +0700

    done

--- src/config/config.js
+++ src/config/config.js
@@ -2,10 +2,8 @@ require('dotenv').config();
 
 const config = {
     TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN || '',
-    CHAT_ID: process.env.CHAT_ID || '',
-    JSON_FILE: process.env.JSON_FILE || 'wb_products.json',
     HOST_CACHE_FILE: process.env.HOST_CACHE_FILE || 'host_cache.json',
-    DEFAULT_NOTIFICATION_INTERVAL: process.env.DEFAULT_NOTIFICATION_INTERVAL || '*/5 * * * *', // Default: every 5 minutes
+    DEFAULT_NOTIFICATION_INTERVAL: process.env.DEFAULT_NOTIFICATION_INTERVAL || '*/5 * * * *',
     MONGODB_URI: process.env.MONGODB_URI
 };
 
